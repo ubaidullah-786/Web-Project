8de766716a459752261d3e5562e338704a536ff4
ubaidullah-786 <ubaidullahashraf786@gmail.com>
2025-06-15 16:50:02 +0500

    added dynamic products

--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 const registerUser = require('../controllers/registrationController');
 const signIn = require('../controllers/signInController');
+const Product = require('../models/productModel');
 
 router.get('/', (req, res) => res.render('homePage'));
 router.get('/contact-us', (req, res) => res.render('contactUs'));
 router.get('/account', (req, res) => res.render('account'));
+
+router.get('/products', async (req, res) => {
+  const products = await Product.find().lean();
+  console.log(products);
+  res.render('products', { products });
+});
+
 router.get('/logout', (req, res) => {
   req.session.destroy(() => {
     res.redirect('/login');
